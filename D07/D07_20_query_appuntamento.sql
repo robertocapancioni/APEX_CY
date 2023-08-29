ed1bc13b1cba1ef5264d3a58aa856344cf2da998
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-08-29 15:57:59 +0200

    Update D07_20_query_appuntamento.sql

--- D07/D07_20_query_appuntamento.sql
+++ D07/D07_20_query_appuntamento.sql
@@ -14,18 +14,16 @@ select
    on a.NOME_ID = n.NOME_ID;
 
 
-create or replace view D07_APPUNTAMENTO_VW as
+create or replace view d07_appuntamento_vw as
 select 
-      a.ID,
-      a.DATA_INIZIO,
-      a.DATA_FINE,
-      a.DESCRIZIONE,
-      a.TIPO_APPUNTAMENTO_ID,
-     ta.TIPO_APPUNTAMENTO, 
-      a.NOME_ID,
-      n.NOME
- from D07_APPUNTAMENTO a
- join D07_TIPO_APPUNTAMENTO ta
-   on a.TIPO_APPUNTAMENTO_ID = ta.TIPO_APPUNTAMENTO_ID
- join D07_NOME n
-   on a.NOME_ID = n.NOME_ID
+ a.ID,
+ a.DATA_INIZIO,
+ a.DATA_FINE,
+ a.DESCRIZIONE,
+ a.TIPO_APPUNTAMENTO_ID,
+ a.NOME_ID,
+ ta.TIPO_APPUNTAMENTO
+ from d07_appuntamento a 
+ join D07_TIPO_APPUNTAMENTO ta on a.tipo_appuntamento_id = ta.tipo_appuntamento_id
+ join d07_nome n on a.nome_id = n.nome_id
+
