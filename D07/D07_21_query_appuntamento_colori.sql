2db3efa8200c9fdd21a13027bb4f8716f99a3dfc
Roberto Capancioni <roberto.capancioni@gmail.com>
2023-08-29 16:31:51 +0200

    Update D07_21_query_appuntamento_colori.sql

--- D07/D07_21_query_appuntamento_colori.sql
+++ D07/D07_21_query_appuntamento_colori.sql
@@ -1,20 +1,20 @@
-select 
-      a.ID,
-      a.DATA_INIZIO,
-      a.DATA_FINE,
-      a.DESCRIZIONE,
-      a.TIPO_APPUNTAMENTO_ID,
-     ta.TIPO_APPUNTAMENTO, 
-      a.NOME_ID,
-      n.NOME,
-      case ta.TIPO_APPUNTAMENTO
+
+CREATE OR REPLACE FORCE EDITIONABLE VIEW "D07_APPUNTAMENTO_COLORE_VW"  AS 
+  select 
+ a.ID,
+ a.DATA_INIZIO,
+ a.DATA_FINE,
+ a.DESCRIZIONE,
+ a.TIPO_APPUNTAMENTO_ID,
+ a.NOME_ID,
+ ta.TIPO_APPUNTAMENTO,
+ n.nome,
+            case ta.TIPO_APPUNTAMENTO
           when 'Primo Appuntamento'  then 'apex-cal-red'
           when 'Altro Appuntamento'  then 'apex-cal-blue'
           when 'Ultimo Appuntamento' then 'apex-cal-green'
       end CLASSE_COLORE
- from D07_APPUNTAMENTO a
- join D07_TIPO_APPUNTAMENTO ta
-   on a.TIPO_APPUNTAMENTO_ID = 
-     ta.TIPO_APPUNTAMENTO_ID
- join D07_NOME n
-   on a.NOME_ID = n.NOME_ID
+ from d07_appuntamento a 
+ join D07_TIPO_APPUNTAMENTO ta on a.tipo_appuntamento_id = ta.tipo_appuntamento_id
+ join d07_nome n on a.nome_id = n.nome_id;
+
